5eb6e90bc7f3b9ee8fb8332b06800feaf233c3f2
Mariana González <gonzalez.marianav19@gmail.com>
2024-07-25 21:38:43 -0600

    primer commit

---
+++ generator/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int edad = 0;
+    cout << "Hola, ingresa tu edaD:";
+    cin >> edad;
+    cout << "Tu edad es:" << edad;
+
+    cout << "Bienvenido al generador de archivos binarios,\n";
+    cout << "";
+
+    return 0;
+}

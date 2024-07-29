1528537df8de2bf21d6a2de24721a9a2e734bede
Mariana González <gonzalez.marianav19@gmail.com>
2024-07-29 17:52:10 -0600

    sirve sin la ruta

--- generator/generator.h
+++ generator/generator.h
@@ -5,62 +5,112 @@
 #include <utility>
 #include <string>
 #include <sstream>
+#include <random>
 
 using namespace std;
-class Generator{
+
+class Generator {
 private:
     string size, outputFile;
 
-    void filegenerator(const string& size, const string& outputfile){
-        ofstream file(outputFile, ios::binary);
+    const int smallSize = 512 * 1024 * 1024; // 512 MB
+    const int mediumSize = 1024 * 1024 * 1024; // 1 GB
+    const int largeSize = 2 * 1024 * 1024 * 1024; // 2 GB
 
-        if (size == "<SMALL>"){
+    int nums[16] = {0b0000, 0b0001, 0b0010, 0b0011, 0b0100, 0b0101, 0b0110, 0b0111,
+                    0b1000, 0b1001, 0b1010, 0b1011, 0b1100, 0b1101, 0b1110, 0b1111};
 
-        } else if (size == "<MEDIUM>"){
+    void filegenerator(const string& size, const string& outputFile) {
+        cout << "alo";
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> dis(0, 15);
+        ofstream file("test.bin", ios::binary);
 
-        }else if (size == "<LARGE>"){
-
-        } else{
-            cout << "No existe archivo de ese tamaño";
+        int a = 0;
+        if (size == "SMALL") {
+            a = smallSize / 4;
+        } else if (size == "MEDIUM") {
+            a = mediumSize / 4;
+        } else if (size == "LARGE") {
+            a = largeSize / 4;
+        } else {
+            cerr << "No existe archivo de ese tamaño" << endl;
+            return;
         }
 
-        int p = 1010;
-        file.write((char*)&p, 4);
+        for (int i = 0; i < a; ++i) {
+            int p = nums[dis(gen)];
+            file.write(reinterpret_cast<char*>(&p), 4);
+        }
 
         file.close();
     }
+
 public:
     Generator() = default;
-    Generator(string  size, string  outputFile)
-    : size(std::move(size)), outputFile(std::move(outputFile)) {}
+    Generator(string size, string outputFile)
+        : size(std::move(size)), outputFile(std::move(outputFile)) {}
 
-    // Métodos para obtener los valores de los atributos
     [[nodiscard]] string getSize() const { return size; }
     [[nodiscard]] string getOutputFile() const { return outputFile; }
 
-    // Métodos para establecer los valores de los atributos
-    void setSize(const string& newSize){
+    void setSize(const string& newSize) {
         size = newSize;
     }
-    void setOutputFile(const string& newOutputFile){
+
+    void setOutputFile(const string& newOutputFile) {
         outputFile = newOutputFile;
     }
 
-    // Método para parsear el comando
     void parseCommand(const string& command) {
         istringstream stream(command);
         string token;
+        string size;
+        string outputFile;
 
         while (stream >> token) {
             if (token == "-size") {
-                stream >> size;
-                cout << size;
+                stream >> token;
+                size = extractValue(token);
+                cout << "Size: " << size << endl;
             } else if (token == "-output") {
-                stream >> outputFile;
-                cout << outputFile;
+                stream >> token;
+                outputFile = extractValue(token);
+                cout << "Output File: " << outputFile << endl;
             }
-            filegenerator(size, outputFile);
         }
+
+        // Trim spaces from extracted values
+        size = trim(size);
+        outputFile = trim(outputFile);
+
+        if (!size.empty() && !outputFile.empty()) {
+            filegenerator(size, outputFile);
+            cout << "llego aquu";
+        } else {
+            cerr << "Error: Invalid size or output file." << endl;
+        }
+    }
+
+    static string extractValue(const string& token) {
+        size_t start = token.find('<');
+        size_t end = token.find('>');
+
+        if (start != string::npos && end != string::npos && start < end) {
+            return token.substr(start + 1, end - start - 1);
+        } else {
+            return "";
+        }
+    }
+
+    static string trim(const string& str) {
+        size_t start = str.find_first_not_of(' ');
+        size_t end = str.find_last_not_of(' ');
+
+        if (start == string::npos) return ""; // no content
+
+        return str.substr(start, end - start + 1);
     }
 };
 
